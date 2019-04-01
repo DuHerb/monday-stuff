06e4375f0df4fddec9365c80dec2f369f3657a3c
Dustin Herboldshimer and Gavin Kimberlin <gavin@>
2019-04-01 16:03:24 -0700

    create a word game that jumbles words and puts them in a sentence.

--- js/wordScripts.js
+++ js/wordScripts.js
@@ -2,9 +2,27 @@ $(document).ready(function(){
 
   $("button").click(function(event){
     event.preventDefault();
-    var userInput = $(".multi-line").val();
+    var userInput = $(".userInput").val();
+
+    console.log(userInput) ;
+    var inputSplit = userInput.split(" ");
+    console.log(inputSplit);
+
+    var bigWords = [];
+    inputSplit.forEach(function(word){
+      if (word.length >= 3){
+        bigWords.push(word);
+      };
+    });
+    console.log(bigWords);
+    var bigRev = bigWords.reverse();
+    console.log(bigRev);
+    var bigString = bigRev.toString();
+    console.log(bigString);
+    var final = "";
+    bigRev.forEach(function(word){
+      final += word + " ";
+    });
+    console.log(final);
   });
-  console.log(userInput) ;
-
-
-})
+});
